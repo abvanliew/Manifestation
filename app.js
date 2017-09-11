19a1daef4291223d0abc15c743cb9168e630935a
abvanliew <ab.vanliew@gmail.com>
2017-09-11 16:11:05 -0400

    Update

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
 var express = require( 'express' );
-//var bodyParser = require( 'body-parser' );
-//var csrf = require( 'csurf' );
-//var session = require( 'client-sessions' );
+var bodyParser = require( 'body-parser' );
 
 var port = process.env.PORT || 3000;
 
 var app = express();
 
 //app.set( 'view engine', 'jade' );
-//app.use( bodyParser.urlencoded( { extended: true } ) );
+app.use( bodyParser.urlencoded( { extended: true } ) );
 
 app.get('/', function(req, res) 
 {
