323e25f7c7e20d103f1114471e34ec92abb784d4
abvanliew <ab.vanliew@gmail.com>
2017-09-11 17:28:53 -0400

    Update

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var express = require( 'express' );
 var bodyParser = require( 'body-parser' );
+var AWS = require('aws-sdk');
+var AWSCognito = require('amazon-cognito-identity-js');
 
 var port = process.env.PORT || 3000;
 
@@ -11,6 +13,21 @@ app.use( bodyParser.urlencoded( { extended: true } ) );
 app.get( '/', function(req, res) 
 {
 	res.render( 'index.jade' );
+});
+
+app.get( '/login', function(req, res) 
+{
+	res.render( 'login.jade' );
+});
+
+app.get( '/register', function(req, res) 
+{
+	res.render( 'register.jade' );
+});
+
+app.get( '/dashboard', function(req, res) 
+{
+	res.render( 'dashboard.jade' );
 });
 
 app.listen( port );
