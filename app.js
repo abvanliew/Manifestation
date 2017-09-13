bc302ae3d4ae802b9a4ad1b4c496b738de49e18e
abvanliew <ab.vanliew@gmail.com>
2017-09-13 18:55:18 -0400

    Update

--- app.js
+++ app.js
@@ -1,31 +1,41 @@
 var express = require( 'express' );
 var bodyParser = require( 'body-parser' );
-var AWS = require('aws-sdk');
-var AWSCognito = require('amazon-cognito-identity-js');
+var AWS = require( 'aws-sdk' );
+var AWSCognito = require( 'amazon-cognito-identity-js' );
 
 var port = process.env.PORT || 3000;
+
+var AWS_REGION = process.env.AWS_REGION;
+//var AWS_ACCOUNT_ID = process.env.AWS_ACCOUNT_ID;
+//var COGNITO_IDENTITY_POOL_ID = process.env.COGNITO_IDENTITY_POOL_ID;
+//var IAM_ROLE_ARN = process.env.IAM_ROLE_ARN;
+//var COGNITO_DATASET_NAME = process.env.COGNITO_DATASET_NAME;
+//var COGNITO_KEY_NAME = process.env.COGNITO_KEY_NAME;
+//var CALLBACKURL = process.env.CALLBACKURL;
+//var AMAZON_CLIENT_ID = process.env.AMAZON_CLIENT_ID;
+//var AMAZON_CLIENT_SECRET = process.env.AMAZON_CLIENT_SECRET;
 
 var app = express();
 
 app.set( 'view engine', 'jade' );
 app.use( bodyParser.urlencoded( { extended: true } ) );
 
-app.get( '/', function(req, res) 
+app.get( '/', function( req, res )
 {
 	res.render( 'index.jade' );
 });
 
-app.get( '/login', function(req, res) 
+app.get( '/login', function( req, res )
 {
 	res.render( 'login.jade' );
 });
 
-app.get( '/register', function(req, res) 
+app.get( '/register', function( req, res )
 {
 	res.render( 'register.jade' );
 });
 
-app.get( '/dashboard', function(req, res) 
+app.get( '/dashboard', function( req, res )
 {
 	res.render( 'dashboard.jade' );
 });
